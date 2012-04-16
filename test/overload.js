d844d31c4b9387c000d666e062f78f343060ea62
James Halliday <mail@substack.net>
2012-04-15 19:48:45 -0700

    expectation of social justice

--- test/overload.js
+++ test/overload.js
@@ -2,7 +2,7 @@ var test = require('tap').test;
 var marx = require('../');
 var totals = require('./lib/totals');
 
-test('overloaded', function (t) {
+test('overloaded inequality', function (t) {
     var work = { web : 20, auth : 5, logger : 1 };
     var workers = { a : 2, b : 1, c : 5 };
     
@@ -17,6 +17,25 @@ test('overloaded', function (t) {
     });
     
     t.same(shares._overflow, { web : 18 });
+    
+    t.end();
+});
+
+test('overloaded equality', function (t) {
+    var work = { web : 20, auth : 20, logger : 20 };
+    var workers = { a : 5, b : 5, c : 5 };
+    
+    var shares = marx(workers, work);
+    t.same(totals(shares), work);
+    
+    Object.keys(workers).forEach(function (worker) {
+        var n = Object.keys(shares[worker]).reduce(function (sum, job) {
+            return sum + shares[worker][job];
+        }, 0);
+        t.ok(n <= workers[worker]);
+    });
+    
+    t.same(shares._overflow, { web : 15, auth : 15, logger : 15 });
     
     t.end();
 });
